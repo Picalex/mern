22d42bbfd40618e09df457e19fae83de7e215db8
alex_note <alex27722@bk.ru>
2020-07-06 15:55:51 +0300

    add edit page

--- client/src/pages/UserEditPage.js
+++ client/src/pages/UserEditPage.js
@@ -1,43 +1,99 @@
-import React, {useCallback, useContext, useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
+import React, {useContext, useEffect, useState} from 'react'
 import {useHttp} from '../hooks/http.hook'
+import {useMessage} from '../hooks/message.hook'
 import {AuthContext} from '../context/AuthContext'
-import {Loader} from '../components/Loader'
-import {UserEdit} from '../components/UserEdit'
 
+export const AuthPage = () => {
+  const auth = useContext(AuthContext)
+  const message = useMessage()
+  const {loading, request, error, clearError} = useHttp()
+  const [form, setForm] = useState({
+    email: '', password: ''
+  })
 
+  useEffect(() => {
+    message(error)
+    clearError()
+  }, [error, message, clearError])
 
-export const UserEditPage = () => {
-    const {token} = useContext(AuthContext)
-    const {request, loading} = useHttp()
-    const [user, setUser] = useState(null)
-    const UserId = useParams().id
+  useEffect(() => {
+    window.M.updateTextFields()
+  }, [])
 
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
 
-    const getUser = useCallback(async () => {
-        try {
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', {...form})
+      message(data.message)
+    } catch (e) {}
+  }
 
-            const fetched = await request(`/api/user/edit/${UserId}`, 'GET', null, {
-                Authorization: `Bearer ${token}`
-            })
-            setUser(fetched)
-        } catch (e) {}
-    }, [token, UserId, request])
+  const loginHandler = async () => {
+    try {
+      const data = await request('/api/auth/login', 'POST', {...form})
+      auth.login(data.token, data.userId)
+    } catch (e) {}
+  }
 
-    useEffect(() => {
-        getUser()
-    }, [getUser])
+  return (
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Вход на портал</h1>
+        <div className="card blue darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">Авторизация</span>
+            <div>
 
-    if (loading) {
-        return <Loader />
-    }
+              <div className="input-field">
+                <input
+                  placeholder="Введите email"
+                  id="email"
+                  type="text"
+                  name="email"
+                  className="yellow-input"
+                  value={form.email}
+                  onChange={changeHandler}
+                />
+                <label htmlFor="email">Email</label>
+              </div>
 
-    return (
-        <>
-            { !loading && user && <UserEdit user={user} /> }
-        </>
+              <div className="input-field">
+                <input
+                  placeholder="Введите пароль"
+                  id="password"
+                  type="password"
+                  name="password"
+                  className="yellow-input"
+                  value={form.password}
+                  onChange={changeHandler}
+                />
+                <label htmlFor="email">Пароль</label>
+              </div>
 
-
-
-    )
+            </div>
+          </div>
+          <div className="card-action">
+            <button
+              className="btn yellow darken-4"
+              style={{marginRight: 10}}
+              disabled={loading}
+              onClick={loginHandler}
+            >
+              Войти
+            </button>
+            <button
+              className="btn grey lighten-1 black-text"
+              onClick={registerHandler}
+              disabled={loading}
+            >
+              Регистрация
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
